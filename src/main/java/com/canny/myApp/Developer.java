98c804c683bd617178168bdf1825d3c81b9f798c
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-07-06 21:20:53 +0300

    added qualifier bean (laptop)

--- src/main/java/com/canny/myApp/Developer.java
+++ src/main/java/com/canny/myApp/Developer.java
@@ -1,10 +1,12 @@
 package com.canny.myApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Developer {
+    @Qualifier("laptop")
     @Autowired //field injection
     private Computer computer;
     public void build() {
