1ad14bea00b6db0fe55bc1bb282159664ee78532
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-07-06 21:01:20 +0300

    laptop implements interface computer

--- src/main/java/com/canny/myApp/Laptop.java
+++ src/main/java/com/canny/myApp/Laptop.java
@@ -3,7 +3,7 @@ package com.canny.myApp;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Laptop {
+public class Laptop implements Computer {
 
     public void compile() {
         System.out.println("Compiling with 313 errors");
